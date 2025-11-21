f14a6297d07d7dd71feac1d02782ccc36e0930b5
Jesús Pastor <86127329+jesuspastr@users.noreply.github.com>
2025-11-21 16:09:47 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -130,6 +130,13 @@ export default function Home({ designers, filters }) {
         <title>Peruvians Who Design</title>
         <link id="favicon" rel="alternate icon" href="/favicon.ico" />
         <MetaTags />
+        {/* Fuentes Geist y Geist Mono desde Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Geist:wght@300;400;500;600;700&family=Geist+Mono:wght@400;500;600&display=swap"
+        rel="stylesheet"
+  />
       </Head>
 
       {!isReady ? (
